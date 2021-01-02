78fc517acf6ed0edda55e4a2b578bdc34466f311
LinkOut <luca.cirillo5@gmail.com>
2021-01-02 22:07:14 +0100

    add: client communicate with the server with both tcp and rmi

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,45 +1,57 @@
 // @author Luca Cirillo (545480)
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.nio.ByteBuffer;
+import java.rmi.NotBoundException;
+import java.rmi.registry.Registry;
 import java.nio.channels.SocketChannel;
-import java.util.Scanner;
+import java.rmi.registry.LocateRegistry;
 
-public class Client {
-    private static final String IP_SERVER = "127.0.0.1";
+public class ClientMain {
+    // * TCP
     private static final int PORT_TCP = 6789;
+    private static final String IP_SERVER = "127.0.0.1";
+    // * RMI
+    private static final int PORT_RMI = 9876;
+    private static final String NAME_RMI = "WORTH-SERVER";
 
-    public static void main(String[] args){
+    public ClientMain(){ }
+
+    private void run(){
         // Creo un SocketChannel per stabilire una connessione TCP
         SocketChannel socketChannel;
 
         try {
             // * TCP Setup
-            // Apro un nuovo SocketChannel
-            socketChannel = SocketChannel.open();
-            // Connetto al server sul canale appena creato
-            socketChannel.connect(new InetSocketAddress(IP_SERVER, PORT_TCP));
-            System.out.println("Connesso a: " + socketChannel.getRemoteAddress());
-
-            Scanner scanner = new Scanner(System.in);
-            ByteBuffer msgBuffer;
-            String msg;
-
-            while(true){ // condizione di uscita su comando
-                // Leggo il messaggio dal terminale
-                System.out.print("Scrivi un messaggio da inviare al server: ");
-                msg = scanner.nextLine();
-                // Lo scrivo in un buffer
-                msgBuffer = ByteBuffer.wrap(msg.getBytes());
-                // E poi passo tutto sul SocketChannel
-                while (msgBuffer.hasRemaining())
-                    socketChannel.write(msgBuffer);
-                // Quindi svuoto il buffer...
-                msgBuffer.clear();
+            try{
+                // Apro un nuovo SocketChannel
+                socketChannel = SocketChannel.open();
+                // Connetto al server sul canale appena creato
+                socketChannel.connect(new InetSocketAddress(IP_SERVER, PORT_TCP));
+                System.out.println("[TCP] Connected to: " + socketChannel.getRemoteAddress());
+            }catch (ConnectException ce){
+                System.err.println("TCP connection refused, are you sure that server is up?");
+                System.exit(-1);
             }
 
-        } catch (IOException ioe) { ioe.printStackTrace(); }
+            // * RMI Setup
+            try{
+                // Creo un registry sulla porta RMI
+                Registry registry = LocateRegistry.getRegistry(PORT_RMI);
+                // Chiamo la lookup sullo stesso nome del server
+                ServerRMI server = (ServerRMI) registry.lookup(NAME_RMI);
+                System.out.println("[RMI] Connected to: " + server.toString());
+            } catch (NotBoundException nbe) {
+                System.err.println("RMI connection refused, are you sure that server is up?");
+                System.exit(-1);
+            }
+
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    public static void main(String[] args){
+        ClientMain client = new ClientMain();
+        client.run();
     }
 }
