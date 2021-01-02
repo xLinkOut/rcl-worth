a41740706f590cf9677d82e8180e7f17bc9d9a59
LinkOut <luca.cirillo5@gmail.com>
2021-01-02 21:51:18 +0100

    add: blank server and server RMI interface

--- src/Server.java
+++ src/Server.java
@@ -1,53 +1,5 @@
 // @author Luca Cirillo (545480)
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.InetSocketAddress;
-import java.nio.channels.Selector;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.ServerSocketChannel;
+public interface Server {
 
-public class Server {
-
-    private static final int PORT_TCP = 6789;
-
-    public static void main(String[] args){
-
-        // Creo le variabili da utilizzare nel try/catch
-        Selector selector = null;
-        ServerSocket serverSocket;
-        ServerSocketChannel serverSocketChannel;
-
-        try{
-            // Creo il ServerSocketChannel, ed implicitamente viene creato un ServerSocket
-            serverSocketChannel = ServerSocketChannel.open();
-            // Configuro il canale per funzionare in modalità non-blocking
-            serverSocketChannel.configureBlocking(false);
-            // Ottengo dal canale il ServerSocket creato implicitamente
-            serverSocket = serverSocketChannel.socket();
-            // Bindo il ServerSocket alla porta TCP sopra specificata
-            serverSocket.bind(new InetSocketAddress(PORT_TCP));
-            // Creo il Selector chiamando la sua .open()
-            selector = Selector.open();
-            // e lo associo al canale, ponendolo in ascolto soltanto sulle "accept"
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-        }catch (IOException ioe){ ioe.printStackTrace(); }
-
-        // Controllo l'effettiva creazione del Selector
-        if(selector == null){
-            System.out.println("Qualcosa è andato storto durante la creazione del Selector!");
-            System.exit(-1);
-        }
-
-        System.out.println("Server in attesa di connessioni sulla porta <"+PORT_TCP+">\n");
-
-        while(true){
-            try {
-                //System.out.println("Aspetto...");
-                // Seleziona un insieme di keys che corrispondono a canali pronti ad eseguire operazioni
-                selector.select();
-            } catch (IOException ioe){ ioe.printStackTrace(); break; }
-            System.out.println(selector.selectedKeys());
-        }
-    }
 }
---
+++ src/ServerRMI.java
@@ -0,0 +1,7 @@
+// @author Luca Cirillo (545480)
+
+import java.rmi.Remote;
+
+public interface ServerRMI extends Remote {
+
+}
