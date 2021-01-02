a2ea872f372a32c75fdb04519bfe4929ee26a954
LinkOut <luca.cirillo5@gmail.com>
2021-01-02 22:23:02 +0100

    add: basic multiplexing

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -2,16 +2,15 @@
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.channels.*;
 import java.rmi.RemoteException;
 import java.net.InetSocketAddress;
-import java.nio.channels.Selector;
 import java.rmi.registry.Registry;
 import java.rmi.server.RemoteObject;
-import java.nio.channels.SelectionKey;
 import java.rmi.AlreadyBoundException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
-import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
 
 public class ServerMain extends RemoteObject implements Server, ServerRMI{
     // * TCP
@@ -26,14 +25,14 @@ public class ServerMain extends RemoteObject implements Server, ServerRMI{
         // Persistenza
     }
 
-    private void live(){
+    private void live() {
         // Variabili da utilizzare nel try/catch
         Selector selector = null;
         ServerSocket serverSocket;
         ServerSocketChannel serverSocketChannel;
 
         // * TCP Setup
-        try{
+        try {
             // Creo il ServerSocketChannel, ed implicitamente viene creato un ServerSocket
             serverSocketChannel = ServerSocketChannel.open();
             // Configuro il canale per funzionare in modalità non-blocking
@@ -46,19 +45,20 @@ public class ServerMain extends RemoteObject implements Server, ServerRMI{
             selector = Selector.open();
             // e lo associo al canale, ponendolo in ascolto soltanto sulle "accept"
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            System.out.println("[TCP] Listening on <"+PORT_TCP+">");
-        }catch (IOException ioe){ ioe.printStackTrace(); } // TODO: Fail fast and exit?
-
-        /* TODO: Controllo ridondante?
-        // Controllo l'effettiva creazione del Selector
-        if(selector == null){
-            System.err.println("Qualcosa è andato storto durante la creazione del Selector!");
+            System.out.println("[TCP] Listening on <" + PORT_TCP + ">");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
             System.exit(-1);
         }
-        */
+
+        // Controllo l'effettiva creazione del Selector
+        if (selector == null) {
+            System.err.println("Something went wrong while creating the Selector!");
+            System.exit(-1);
+        }
 
         // * RMI Setup
-        try{
+        try {
             // Esporto l'oggetto remoto <ServerRMI>
             ServerRMI stub = (ServerRMI) UnicastRemoteObject.exportObject(this, 0);
             // Creo il registro alla porta RMI specificata
@@ -66,9 +66,52 @@ public class ServerMain extends RemoteObject implements Server, ServerRMI{
             // Bindo lo stub al registry così da poter essere individuato
             // da altri hosts attraverso il nome <NAME_RMI>
             registry.bind(NAME_RMI, stub);
-            System.out.println("[RMI] Bound on <"+NAME_RMI+">");
-        } catch (RemoteException | AlreadyBoundException e) { e.printStackTrace(); }
+            System.out.println("[RMI] Bound on <" + NAME_RMI + ">");
+        } catch (RemoteException | AlreadyBoundException e) {
+            e.printStackTrace();
+            System.exit(-2);
+        }
 
+        // Serve forever
+        while (true) {
+            try {
+                // Seleziona un insieme di keys che corrispondono a canali pronti ad eseguire operazioni
+                selector.select();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+                break;
+            }
+
+            // Iteratore sui canali che risultano pronti
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+
+            while (iterator.hasNext()) {
+                // Prendo dall'iteratore la prossima key
+                SelectionKey key = iterator.next();
+                // La rimuovo esplicitamente dall'iteratore
+                iterator.remove();
+
+                // Se la key considerata identifica:
+                try {
+
+                    // Un canale pronto ad accettare una nuova connessione
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel socket = (ServerSocketChannel) key.channel();
+                        // Accetto la connessione
+                        SocketChannel client = socket.accept();
+                        System.out.println("<" + client.getRemoteAddress() + ">: Connessione accettata");
+                        client.configureBlocking(false);
+                        // Registro il nuovo client sul Selector
+                        client.register(selector, SelectionKey.OP_READ, null);
+                    }
+
+                } catch (ClosedChannelException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static void main(String[] args){
