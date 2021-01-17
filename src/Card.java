9d4d7819094c51ba4c1a73ccf116f3039e74d638
LinkOut <luca.cirillo5@gmail.com>
2021-01-17 18:50:08 +0100

    cln: code cleanup

--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,11 @@
+// @author Luca Cirillo (545480)
 
 public class Card {
 
-    private final String name;        // Nome della Card (univoco nell'ambito del progetto)
+    private final String name;        // Nome della Card, univoco nell'ambito del progetto
     private final String description; // Descrizione della Card
-
     private String history;           // Storia dei movimenti, definita come Section1|...|SectionN
-    private Project.Section section;  // Lista in cui si trova attualmente la Card
+    private Project.Section section;  // Riferimento alla lista in cui si trova attualmente la Card
 
     public Card(String name, String description){
         this.name = name;
@@ -13,24 +13,20 @@ public class Card {
         this.section = Project.Section.TODO;
         this.history = Project.Section.TODO.toString() + "|";
     }
-
+    
+    // Getters
     public String getName() { return name; }
-
     public String getDescription() { return description; }
-
+    public String getHistory() { return history; }
     public Project.Section getSection() { return section; }
-
+    
+    // Setters
     public void setSection(Project.Section section) {
         this.section = section;
-        addHistory(section);
+        this.history += section.toString() + "|";
     }
 
-    public String getHistory() { return history; }
-
-    public void setHistory(String history) { this.history += history + "|"; }
-
-    private void addHistory(Project.Section section) {this.history += section.toString() + "|"; }
-
+    // toString, usato principalmente da showCard
     public String toString(){
         return
             "- Name > " + this.name + "\n" +
