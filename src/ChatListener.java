5221fbcd3d773d55ba75432520ca627bb136636b
LinkOut <luca.cirillo5@gmail.com>
2021-01-23 23:37:22 +0100

    chg: all code under the same try/catch block

--- src/ChatListener.java
+++ src/ChatListener.java
@@ -7,8 +7,6 @@ import java.net.DatagramPacket;
 import java.net.MulticastSocket;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-// TODO: riunire tutto sotto lo stesso try/catch
-
 // Thread in ascolto sulla chat di progetto per ricevere i messaggi
 public class ChatListener implements Runnable{
 
@@ -36,45 +34,24 @@ public class ChatListener implements Runnable{
 
     @Override
     public void run() {
-        // Dichiaro fuori dal try/catch tutto il necessario
-        InetAddress inetAddress = null;
-        MulticastSocket multicastSocket = null;
-        DatagramPacket packet = null;
-        try {
-            inetAddress = InetAddress.getByName(multicastIP);
-            multicastSocket = new MulticastSocket(multicastPort);
+        try{
+            InetAddress inetAddress = InetAddress.getByName(multicastIP);
+            MulticastSocket multicastSocket = new MulticastSocket(multicastPort);
             // Mi unisco al gruppo
             multicastSocket.joinGroup(inetAddress);
-            packet = new DatagramPacket(new byte[8192],8192);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-            System.out.println("Something went wrong while trying to reach the project chat ...");
-        }
-
-        // Controllo "ridondante" per evitare warning
-        if(multicastSocket == null || packet == null){
-            System.out.println("Something went wrong while trying to reach the project chat ...");
-            return;
-        }
-
-        try{
+            DatagramPacket packet = new DatagramPacket(new byte[8192],8192);
             while(!Thread.currentThread().isInterrupted()) {
                 // Ricevo nuovi messaggi da altri utenti
                 multicastSocket.receive(packet);
                 // Li aggiungo alla coda
                 messagesQueue.add(new String(packet.getData(),0,packet.getLength()));
             }
-        } catch (IOException ioe) {
-            System.out.println("Something went wrong while I was waiting for messages on the chat ...");
-        }
-
-
-        try {
             // Alla fine, abbandono il gruppo
             multicastSocket.leaveGroup(inetAddress);
             // e chiudo il socket
             multicastSocket.close();
-        } catch (IOException ignored) {}
-
+        } catch (IOException e) {
+            System.out.println("Something went wrong while trying to reach the project chat ...");
+        }
     }
 }
